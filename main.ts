1e4af88b7a49ac490389db87ae60492fd7b3887a
heyymuhammad <muawan541@gmail.com>
2024-03-30 15:30:35 +0500

    Tried solution for "file not found" error. Also added comments in the code for better understanding.

--- main.ts
+++ main.ts
@@ -7,6 +7,8 @@ let date: Date = new Date();
 // Reading PIN from file
 const pinStr = fs.readFileSync("pin.txt", 'utf8');
 let pin = parseInt(pinStr);
+
+// Reading Balance from the last transaction history
 let transactionHistory = fs.readFileSync("transaction.csv", 'utf8');
 let readBalance
 if (transactionHistory.length > 10) {
@@ -16,7 +18,7 @@ if (transactionHistory.length > 10) {
 
 let balance = readBalance?parseInt(readBalance[2]):0;
 let transactionAmount, transactionMethod;
-
+// User Login
 const answer = await inquirer.prompt([
     {
         name: "userPin",
@@ -24,7 +26,7 @@ const answer = await inquirer.prompt([
         message: "Enter your PIN: ",
     }
 ]);
-
+// Verifying login and Showing dashboard to the user
 if (answer.userPin == pin) {
     console.log("Correct Pin Code");
     let operationAns = await inquirer.prompt([
@@ -35,6 +37,7 @@ if (answer.userPin == pin) {
         choices: ["Withdraw", "Check Balance", "Fast Cash", "Change PIN", "View Transaction History", "Transfer Money"]
     }
     ]);
+    //Withdraw feature
     if (operationAns.operation == "Withdraw") {
         let withdrawAmnt = await inquirer.prompt([
         {
@@ -43,6 +46,7 @@ if (answer.userPin == pin) {
             message: "Enter amount: "
         }
         ]);
+        // Verifying required amount in account before withdraw
         if (withdrawAmnt.amount <= balance) {
             transactionAmount = withdrawAmnt.amount;
             let wantSlip = await inquirer.prompt([   
@@ -53,12 +57,14 @@ if (answer.userPin == pin) {
                 choices: ["Yes", "No"]
             }
             ]);
+            // Asking for the Slip
             if (wantSlip.answer=="Yes") {
                 console.log(`Branch Name: Shahra e Faisal Branch`);
                 console.log(`Previous Balance: ${balance} PKR`);
                 console.log(`Withdraw Amount: ${withdrawAmnt.amount} PKR`);
                 console.log(`${date}`);
             }
+            // Modifying balance after successful withdraw
             balance -= withdrawAmnt.amount;
             console.log(`Your remaining balance is: ${balance} PKR`);
         }
@@ -66,10 +72,13 @@ if (answer.userPin == pin) {
             console.log("Insufficient balance.");
         }
     }
+    // Check balance feature
     else if (operationAns.operation == "Check Balance") {
         console.log(`Your current balance is: ${balance} PKR`);
     }
+    // Fast cash feature
     else if (operationAns.operation == "Fast Cash") {
+        // User can only choose limited options for fast cash
         let fastCashAmnt = await inquirer.prompt([
             {
                 name: "amount",
@@ -77,6 +86,7 @@ if (answer.userPin == pin) {
                 choices: [1000, 5000, 10000, 15000, 20000, 25000, 30000]
             }
         ]);
+        // Verifying required amount in account before withdraw
         if (fastCashAmnt.amount <= balance) {
             transactionAmount = fastCashAmnt.amount;
             let wantSlip = await inquirer.prompt([   
@@ -87,12 +97,14 @@ if (answer.userPin == pin) {
                 choices: ["Yes", "No"]
             }
             ]);
+            // Asking for the Slip
             if (wantSlip.answer=="Yes") {
                 console.log(`Branch Name: Shahra e Faisal Branch`);
                 console.log(`Previous Balance: ${balance} PKR`);
                 console.log(`Withdraw Amount: ${fastCashAmnt.amount} PKR`);
                 console.log(`${date}`);
             }
+            // Modifying balance after successful withdraw
             balance -= fastCashAmnt.amount;
             console.log(`Your remaining balance is: ${balance} PKR`);
         }
@@ -100,7 +112,9 @@ if (answer.userPin == pin) {
             console.log("Insufficient balance.");
         }
     }
+    // Change PIN feature
     else if (operationAns.operation == "Change PIN") {
+        // Verifying before changing PIN
         let oldPin = await inquirer.prompt([
             {
                 name: "userPin",
@@ -108,6 +122,7 @@ if (answer.userPin == pin) {
                 message: "Enter your old PIN: ",
             }
         ]);
+        // After verification updating pin in pin.txt file
         if (oldPin.userPin == pin) {
             let newPin = await inquirer.prompt([
                 {
@@ -120,10 +135,13 @@ if (answer.userPin == pin) {
             console.log("Your PIN is successfully changed.");
         } 
     }
+    // View transaction history feature
     else if (operationAns.operation ==  "View Transaction History") {
         transactionHistory?console.log(transactionHistory):console.log("You have not done any transaction.");
     }
+    // Transfer money feature
     else if (operationAns.operation == "Transfer Money") {
+        // Verifying before money transfer
         let verifyPin = await inquirer.prompt([
             {
                 name: "userPin",
@@ -131,6 +149,7 @@ if (answer.userPin == pin) {
                 message: "Enter PIN to verify: ",
             }
         ]);
+        // After verification user needs to provide info about the recieving account
         if (verifyPin.userPin == pin) {
             console.log("Please enter the information of the recieving account.")
             let receivingAccount = await inquirer.prompt([
@@ -150,12 +169,15 @@ if (answer.userPin == pin) {
                     message: "Amount (Enter amount between 100 PKR - 50,000 PKR): "
                 }
             ]);
+            // Incase of more transfer amount than current balance
             if (receivingAccount.transferAmnt > balance) {
                 console.log(`Insufficient Balance. Please enter amount between not more than ${balance}.`);
             }
+            // Incase of incorrect informations
             else if (receivingAccount.accountNo.length != 12 || !receivingAccount.accountName) {
                 console.log("Incorrect information. Try Again!");
             }
+            // When everything is perfectly fine
             else {
                 console.log("Your amount has been transfered successfully to the following account.\n");
                 console.log(`Account No: ${receivingAccount.accountNo}`);
@@ -168,6 +190,7 @@ if (answer.userPin == pin) {
             }
         }
     }
+    // Adding transaction history to transaction.csv file
     if (transactionAmount) {
         const transaction = `${transactionAmount},${date},${balance},${transactionMethod?transactionMethod:"Debited"}`;
         fs.appendFileSync("transaction.csv", transaction + '\n', 'utf8');
